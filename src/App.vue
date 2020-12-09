4cb643d7dd8f24411a04061181fc2a1bb39ac8ee
zhouxiaofei <zhouxiaofei0001@gmail.com>
2020-12-09 18:26:13 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>CV翻译</h1>
+    <div class="container">
+
+      <div class="input1">
+        <h2 class="text_area_label1">Android</h2>
+        <textarea class="from_input1" placeholder="输入安卓待翻译文本">
+        </textarea>
+        <h2 class="label2">iOS</h2>
+        <textarea class="from_input2" placeholder="输入安卓待翻译文本">
+        </textarea>
+      </div>
+      <button class="convert_button button"><p>翻译</p><img class="button_img" src="@/assets/arrow-right.png"></button>
+      <div class="output_container">
+
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -25,4 +36,66 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.output_container {
+  background: aqua;
+  width: 800px;
+
+}
+
+.button_img {
+  width: 20px;
+  height: 20px;
+}
+
+.button {
+  width: 100px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  flex-flow: row;
+  justify-content: space-around;
+}
+
+.convert_button {
+  margin-right: 10px;
+}
+
+.container {
+  width: 100%;
+  height: 800px;
+  background: #42b983;
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.input1 {
+  display: flex;
+  flex-flow: column;
+  align-items: start;
+  width: 800px;
+}
+
+.text_area_label1 {
+  margin-left: 20px;
+}
+.label2 {
+  margin-left: 20px;
+}
+
+
+.from_input1 {
+  width: 400px;
+  height: 200px;
+  resize: none;
+}
+.from_input2 {
+  width: 400px;
+  height: 200px;
+  resize: none;
+}
+
+
 </style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <div class="container">
+      <h1>CV翻译</h1>
+    <div class="input1">
+      <h2 class="text_area_label1">Android</h2>
+      <textarea class="from_input1" placeholder="输入安卓待翻译文本">
+
+      </textarea>
+
+      <h2 class="label2">iOS</h2>
+      <textarea class="from_input2" placeholder="输入安卓待翻译文本">
+
+      </textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+}
+</script>
+
+<style scoped>
+
+  .container {
+    width: 100%;
+    width: 800px;
+    background: #42b983;
+  }
+
+  .input1 {
+    display: flex;
+    flex-flow: column;
+    align-items: start;
+  }
+
+  .text_area_label1 {
+    margin-left: 20px;
+  }
+  .label2 {
+    margin-left: 20px;
+  }
+
+
+  .from_input1 {
+    width: 400px;
+    height: 200px;
+    resize: none;
+  }
+  .from_input2 {
+    width: 400px;
+    height: 200px;
+    resize: none;
+  }
+
+
+</style>
